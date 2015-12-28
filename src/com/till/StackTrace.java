7aed6c5bce39ce9a517a5bdce0276c7583822c2e
Piotr Droś <piotrdros@gmail.com>
2015-12-28 18:49:38 +0100

    komit

---
+++ src/com/till/StackTrace.java
@@ -0,0 +1,34 @@
+package com.till;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class StackTrace {
+
+	public static String methodName() {
+		StringWriter sw = new StringWriter();
+		new Throwable().printStackTrace(new PrintWriter(sw));
+		String stackTrace = sw.toString();
+		String[] split = stackTrace.split("\n");
+		for (int i = 0; i < split.length; i++) {
+			if (split[i].contains("com.till.StackTrace.methodName")) {
+				return split[i + 1].replace("at ", "").trim();
+			}
+		}
+		return "ERROR";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(methodName());
+
+		// Object s = new Object() {
+		// @Override
+		// public String toString() {
+		// return methodName();
+		// };
+		// };
+
+		// System.out.println(s);
+	}
+
+}
